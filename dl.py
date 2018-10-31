a3a5e82fd83e1063ded476703c73db7da1375cd3
Michał Chęciński <michal.checinski@outlook.com>
2018-10-31 17:27:46 +0100

    Working version (at dev machine):
    using redis for user creds
    split app into two apps:
      * first for files up&download - dl - as a API
      * second for serving html pages and manage users data
      Communication between them is based on jwt

--- dl.py
+++ dl.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask import session
 from flask import request
 from flask import make_response
 from flask import send_from_directory
@@ -8,54 +7,83 @@ from werkzeug.utils import secure_filename
 import os, os.path
 import json
 from flask import jsonify
+import jwt
+from functools import wraps
+from datetime import datetime
 
-app = Flask(__name__, static_url_path='/checinsm/file/static')
+app = Flask(__name__)
 app.secret_key = b'jf764o;03n?mv9936bv?le874nb;s.'
+app.config['SECRET_KEY'] = 'giNyIS8Tc9oQR1GIiq6nvhyzg9MOkvMHBilwv16W_rE'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #16MB max upload size
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+
+        token = request.form.get('jwt') or request.args.get('jwt')
+
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = data['username']
+            expiration = datetime.utcfromtimestamp(data['exp'])
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+
+        if expiration < datetime.utcnow():
+            return jsonify({'message' : 'Token is invalid! Token Expired!'}), 401
+
+        if not current_user:
+            return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
 
 @app.route('/checinsm/dl/files/<username>/<filename>', methods=['GET'])
-def download(username, filename):
-    cookie_username = username_from_cookie(request.cookies.get('userID'))
-    if cookie_username is None:
-        abort(401)
-    if cookie_username != username:
-        abort(403)
+@token_required
+def download(current_user, username, filename):
+    if current_user != username:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
     root_dir = os.path.dirname(os.getcwd())+'/file'
     return send_from_directory(os.path.join(root_dir, 'files', username), filename, as_attachment=True)
 
 
 @app.route('/checinsm/dl/upload', methods=['POST'])
-def upload():
-    username = username_from_cookie(request.cookies.get('userID'))
-    if username is None:
-        abort(401)
-
+@token_required
+def upload(current_user):
     print(request.files)
-    if can_upload_files(username) == False:
-        return "You cannot upload more files"
+    if can_upload_files(current_user) == False:
+        return make_response("You cannot upload more files", 403)
     if request.method == 'POST':
         f = request.files['file']
-        f.save('files/' + username +'/' + secure_filename(f.filename))
-    return "Success"
+        f.save('files/' + current_user +'/' + secure_filename(f.filename))
+    return make_response('Success', 200)
 
 
-@app.route('/checinsm/dl/list', methods=['GET'])
-def list():
-    username = username_from_cookie(request.cookies.get('userID'))
-    if username is None:
-        abort(401)
-
-    files = files_list(username)
-    return jsonify(files)
+# @app.route('/checinsm/dl/canUpload', methods=['GET'])
+# @token_required
+# def can_upload(current_user):
+#     return jsonify({"can_upload" : can_upload_files(current_user)})
 
 
-def username_from_cookie(cookie):
-    with open('users.json', 'r') as j:
-        datastore = json.load(j)
+# @app.route('/checinsm/dl/list', methods=['GET'])
+# @token_required
+# def list(current_user):
+#     files = files_list(current_user)
 
-    for row in datastore['users']:
-        if row['token'] == cookie:
-            return row['username']
-    return None
+#     output = []
+
+#     for file in files:
+#         file_data = {}
+#         file_data["url"] = f'{request.url_root}checinsm/dl/files/{current_user}/{file}'
+#         file_data["filename"] = file
+#         output.append(file_data)
+#     return jsonify({'files': output})
 
 
 def can_upload_files(username):
@@ -65,14 +93,12 @@ def can_upload_files(username):
     return False
 
 
-def can_upload(username, files):
-    if len(files) < 5:
-        return True
-    return False
-
-    
 def files_list(username):
     path = './files/'+username
     if not os.path.exists(path):
         os.makedirs(path)
     return os.listdir(path)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=4500, debug=True)
--- file.py
+++ file.py
@@ -1,18 +1,44 @@
 from flask import Flask
-from flask import session
 from flask import request
 from flask import render_template
 from flask import make_response
 from flask import redirect
 from flask import url_for
-from flask import flash
-from flask import send_from_directory
-from werkzeug.utils import secure_filename
 import os, os.path
 import json
+import string
+import random
+import redis
+import hashlib
+import jwt
+import datetime
+import requests 
 
-app = Flask(__name__, static_url_path='/checinsm/file/static')
-app.secret_key = b'jf764o;03n?mv9936bv?le874nb;s.'
+curr_dir = os.getcwd()
+
+print(os.path.join(curr_dir, 'static'))
+
+app = Flask(__name__, static_url_path='/static/')
+app.secret_key = b'jf764o;do7?mv9936bv?lkgt67;s.'
+
+app.config['SECRET_KEY'] = 'giNyIS8Tc9oQR1GIiq6nvhyzg9MOkvMHBilwv16W_rE'
+app.config['base_api_url'] = 'http://localhost:4500/checinsm/dl'
+app.config["base_app_url"] = 'http://localhost:5000/checinsm/file'
+
+redis = redis.Redis()
+
+def register_user(username, password):
+    if redis.get('checinsm:user:'+username+':password'):
+        return False
+    salt = ''.join(random.choices(string.ascii_letters+string.digits, k=16))
+    passwd = hashlib.sha3_256(password.encode('utf-8')+salt.encode('utf-8')).hexdigest()
+    print(passwd)
+    redis.set('checinsm:user:'+username+':password', passwd)
+    redis.set('checinsm:user:'+username+':salt', salt)
+    return True
+
+register_user('michal', 'haslo')
+
 
 @app.route('/checinsm/file/')
 def index():
@@ -34,37 +60,28 @@ def login():
     return render_template('login.html', error=error)
 
 
+@app.route('/checinsm/file/register', methods=['POST', 'GET'])
+def register():
+    username = username_from_cookie(request.cookies.get('userID'))
+    if username:
+        return redirect(url_for('list'))
+        
+    error = None
+    if request.method == 'POST':
+        if register_user(request.form['username'], request.form['password']):
+            return redirect(url_for('login'))
+        else:
+            error = 'Username exists in app already'
+    return render_template('register.html', error=error)
+
+
 @app.route('/checinsm/file/logout', methods=['GET'])
 def logout():
+    username = username_from_cookie(request.cookies.get('userID'))
+    delete_user_token(username)
     resp = make_response(render_template('logout.html'))
     resp.set_cookie('userID', '', expires=0, secure=True, httponly=True)
     return resp
-
-
-@app.route('/checinsm/file/files/<username>/<filename>', methods=['GET'])
-def download(username, filename):
-    cookie_username = username_from_cookie(request.cookies.get('userID'))
-    if cookie_username is None:
-        return render_template('login.html', error='Musisz sie najpierw zalogować')
-    if cookie_username != username:
-        return render_template('fileerr.html', error='Nie ładnie tak grzebać w nieswoich plikach!')
-    root_dir = os.path.dirname(os.getcwd())+'/file'
-    return send_from_directory(os.path.join(root_dir, 'files', username), filename, as_attachment=True)
-
-
-@app.route('/checinsm/file/upload', methods=['POST'])
-def upload():
-    username = username_from_cookie(request.cookies.get('userID'))
-    if username is None:
-        return render_template('login.html', error='Musisz sie najpierw zalogować')
-
-    print(request.files)
-    if can_upload_files(username) == False:
-        return "You cannot upload more files"
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save('files/' + username +'/' + secure_filename(f.filename))
-    return redirect(url_for('list'))
 
 
 @app.route('/checinsm/file/list')
@@ -73,53 +90,61 @@ def list():
     if username is None:
         return render_template('login.html', error='Musisz sie najpierw zalogować')
 
-    files = files_list(username)
-    return render_template('list.html', files=files, username=username, can_upload=can_upload(username, files))
+    files = files_name_url(username)
+
+    return render_template('list.html', files=files, can_upload=can_upload(username), jwt=make_jwt(username), upload_url=app.config['base_api_url']+"/upload")
+
+
+@app.route('/checinsm/file/files/<username>/<filename>', methods=['GET'])
+def download(username, filename):
+    cookie_username = username_from_cookie(request.cookies.get('userID'))
+    if cookie_username is None:
+        return render_template('login.html', error='Musisz sie najpierw zalogować')
+    if cookie_username != username:
+        return render_template('fileerr.html', error='NIeładnie tak pobierać nieswoje pliki!')
+    jwt = make_jwt(username)
+    return redirect(f'{app.config["base_api_url"]}/files/{username}/{filename}?jwt={jwt}', code=301)
 
 
 def valid_login(username, password):
-    with open('users.json', 'r') as j:
-        datastore = json.load(j)
+    redis_password = redis.get('checinsm:user:'+username+':password').decode('utf-8')
+    redis_salt = redis.get('checinsm:user:' + username + ':salt').decode('utf-8')
 
-    for row in datastore['users']:
-        if row['username'] == username:
-            if row['password'] == password:
-                return True
+    if not redis_password or not redis_salt:
+        return False
+
+    hashed_password = hashlib.sha3_256(password.encode('utf-8') + redis_salt.encode('utf-8')).hexdigest()
+    if redis_password == hashed_password:
+        return True
     return False
 
 
 def log_the_user_in(username):
     import datetime
-    expire_date = datetime.datetime.now()
-    expire_date = expire_date + datetime.timedelta(days=30)
-    cookie = cookie_insert(username)
+    expire_date = datetime.datetime.now() + datetime.timedelta(days=1)
+    cookie = insert_user_token(username)
     resp = redirect(url_for('list'))
-    resp.set_cookie('userID', cookie, expires=expire_date, secure=True, httponly=True)
+    resp.set_cookie('userID', cookie, expires=expire_date)#, secure=True, httponly=True)
     return resp
 
 
-def cookie_insert(username):
+def insert_user_token(username):
     token = token_generate()
-    with open('users.json', 'r') as j:
-        datastore = json.load(j)
-
-    for row in datastore['users']:
-        if row['username'] == username:
-            row['token'] = token
-
-    with open('users.json', 'w') as f:
-        json.dump(datastore, f)
-
+    redis.set('checinsm:user:'+username+':token', token)
     return token
 
 
-def username_from_cookie(cookie):
-    with open('users.json', 'r') as j:
-        datastore = json.load(j)
+def delete_user_token(username):
+    if not username:
+        return
+    redis.set('checinsm:user:'+username+':token', '')
 
-    for row in datastore['users']:
-        if row['token'] == cookie:
-            return row['username']
+
+def username_from_cookie(cookie):
+    for key in redis.scan_iter('checinsm:user:*:token'):
+        redis_token = redis.get(key).decode('utf-8')
+        if redis_token == cookie:
+            return key.decode('utf-8').replace('checinsm:user:', '').replace(':token', '')
     return None
 
 
@@ -128,21 +153,38 @@ def token_generate():
     return str(uuid.uuid4().hex)
 
 
-def can_upload_files(username):
+def can_upload(username):
     files = files_list(username)
     if len(files) < 5:
         return True
     return False
 
 
-def can_upload(username, files):
-    if len(files) < 5:
-        return True
-    return False
-
-    
 def files_list(username):
     path = './files/'+username
     if not os.path.exists(path):
         os.makedirs(path)
     return os.listdir(path)
+
+
+def files_name_url(username):
+    files = files_list(username)
+
+    output = []
+
+    for file in files:
+        file_data = {}
+        file_data["url"] = f'{app.config["base_app_url"]}/files/{username}/{file}'
+        file_data["filename"] = file
+        output.append(file_data)
+
+    return output
+
+
+def make_jwt(username, expiration_minutes=3):
+    expiry = datetime.datetime.utcnow() + datetime.timedelta(minutes=expiration_minutes)
+    return jwt.encode({'username': username, 'exp' : expiry}, app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=False)
