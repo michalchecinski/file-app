dc2736702299632f9a42c04166133c6226066406
Michał Chęciński <michal.checinski@outlook.com>
2019-01-16 10:32:19 +0100

    Add rabbitmq support for minify images

--- dl.py
+++ dl.py
@@ -9,13 +9,25 @@ from flask import jsonify
 import jwt
 from functools import wraps
 from datetime import datetime
+import requests
+import json
+import pika
+
+
+with open('config.json') as f:
+    config = json.load(f)
 
 
 app = Flask(__name__)
 app.secret_key = b'jf764o;03n?mv9936bv?le874nb;s.'
-app.config['SECRET_KEY'] = 'giNyIS8Tc9oQR1GIiq6nvhyzg9MOkvMHBilwv16W_rE'
-app.config["base_app_url"] = 'http://localhost:5000/checinsm/file'
+app.config['SECRET_KEY'] = config['SECRET_KEY']
+app.config["base_app_url"] = config["base_app_url"]
+app.config["notification_url"] = config["notification_url"]
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #16MB max upload size
+
+exchange = 'checinsm-minify'
+exchange_type = 'direct'
+routing_key = ''
 
 def token_required(f):
     @wraps(f)
@@ -47,7 +59,7 @@ def token_required(f):
 @app.route('/checinsm/dl/files/<username>/<filename>', methods=['GET'])
 @token_required
 def download(current_user, username, filename):
-    if current_user != username:
+    if current_user != username and current_user != 'shared':
         return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
     root_dir = os.path.dirname(os.getcwd())+'/file'
     return send_from_directory(os.path.join(root_dir, 'files', username), filename, as_attachment=True)
@@ -60,30 +72,13 @@ def upload(current_user):
         return make_response("You cannot upload more files", 403)
     if request.method == 'POST':
         f = request.files['file']
-        f.save('files/' + current_user +'/' + secure_filename(f.filename))
+        path = 'files/' + current_user +'/' + secure_filename(f.filename)
+        f.save(path)
+        data = {'filename':secure_filename(f.filename)}
+        requests.post(app.config["notification_url"]+'/'+current_user, data=data, verify=False)
+        send_minify(path, f.filename)
     #return make_response('Success', 200)
     return redirect(f'{app.config["base_app_url"]}/list', code=301)
-
-
-# @app.route('/checinsm/dl/canUpload', methods=['GET'])
-# @token_required
-# def can_upload(current_user):
-#     return jsonify({"can_upload" : can_upload_files(current_user)})
-
-
-# @app.route('/checinsm/dl/list', methods=['GET'])
-# @token_required
-# def list(current_user):
-#     files = files_list(current_user)
-
-#     output = []
-
-#     for file in files:
-#         file_data = {}
-#         file_data["url"] = f'{request.url_root}checinsm/dl/files/{current_user}/{file}'
-#         file_data["filename"] = file
-#         output.append(file_data)
-#     return jsonify({'files': output})
 
 
 def can_upload_files(username):
@@ -99,6 +94,19 @@ def files_list(username):
         os.makedirs(path)
     return os.listdir(path)
 
+def send_minify(path, filename):
+    if filename.endswith(('.png', '.jpg', '.jpeg')):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=4500, debug=True)
+        channel.exchange_declare(exchange=exchange,
+                                exchange_type=exchange_type)
+        channel.basic_publish(exchange=exchange,
+                            routing_key=routing_key,
+                            body=path)
+        connection.close()
+
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 4500)
